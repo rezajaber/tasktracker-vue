619269401eb71ca0dc5d8fb6db08632f5f94c717
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-13 01:06:49 +0200

    add CardDetails - Layout

---
+++ src/components/CardDetails.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+import { CalendarClock } from "lucide-vue-next";
+import { Info } from "lucide-vue-next";
+import { Layers } from "lucide-vue-next";
+import { GalleryVerticalEnd } from "lucide-vue-next";
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-between">
+      <div class="flex gap-2">
+        <Button type="submit"
+          ><GalleryVerticalEnd class="mr-1.5 w-5" />Priority</Button
+        >
+        <Button type="submit"><Layers class="mr-1.5 w-5" />Category</Button>
+      </div>
+
+      <div class="flex gap-2">
+        <Button type="submit"><CalendarClock class="mr-1.5 w-5" />Date</Button>
+        <Button type="submit"><Info class="mr-1.5 w-5" />Infos</Button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
-import Card from "@/components/Card.vue";
 import SearchBar from "@/components/SearchBar.vue";
+import CardDetails from "@/components/CardDetails.vue";
+import Card from "@/components/Card.vue";
 </script>
 
 <template>
   <div class="flex h-screen items-center justify-center">
     <div class="grid gap-6">
-      <SearchBar />
+      <div class="grid gap-2">
+        <SearchBar />
+        <CardDetails />
+      </div>
       <Card />
     </div>
   </div>
