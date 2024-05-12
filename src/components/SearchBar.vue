1a1e3f51ec520867f56a80fe6b5996d259a21691
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-13 00:20:44 +0200

    add Search/CreateBar & Filter

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Search } from "lucide-vue-next";
+import { CirclePlus } from "lucide-vue-next";
+import { SlidersHorizontal } from "lucide-vue-next";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <div class="flex justify-between">
+    <div class="flex gap-2">
+      <div class="relative w-96 max-w-md items-center">
+        <Input
+          id="search"
+          type="text"
+          placeholder="Create or Search for a Task..."
+          class="pl-10"
+        />
+        <span
+          class="absolute inset-y-0 start-0 flex items-center justify-center px-3"
+        >
+          <Search class="w-5 text-muted-foreground" />
+        </span>
+      </div>
+
+      <Button type="submit"
+        ><CirclePlus class="mr-1.5 w-4" />Create Task</Button
+      >
+    </div>
+
+    <Button type="submit"
+      ><SlidersHorizontal class="mr-1.5 w-4" />Filter</Button
+    >
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import Card from "@/components/Card.vue";
+import SearchBar from "@/components/SearchBar.vue";
 </script>
 
 <template>
   <div class="flex h-screen items-center justify-center">
-    <Card />
+    <div class="grid gap-6">
+      <SearchBar />
+      <Card />
+    </div>
   </div>
 </template>
