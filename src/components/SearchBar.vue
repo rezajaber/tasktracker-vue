a118ea65e76794076cd877dd96c9cc1f4be7cb4d
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-13 13:44:41 +0200

    add filter-dropdown - multiple select

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 import { Search } from "lucide-vue-next";
 import { CirclePlus } from "lucide-vue-next";
 import { SlidersHorizontal } from "lucide-vue-next";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import type { DropdownMenuCheckboxItemProps } from "radix-vue";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuCheckboxItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+type Checked = DropdownMenuCheckboxItemProps["checked"];
+
+const showDone = ref<Checked>(false);
+const overdue = ref<Checked>(false);
+const critical = ref<Checked>(false);
+const high = ref<Checked>(false);
+const normal = ref<Checked>(false);
+const low = ref<Checked>(false);
 </script>
 
 <template>
@@ -29,8 +51,37 @@ import { Button } from "@/components/ui/button";
       >
     </div>
 
-    <Button type="submit"
-      ><SlidersHorizontal class="mr-1.5 w-4" />Filter</Button
-    >
+    <DropdownMenu>
+      <DropdownMenuTrigger as-child>
+        <Button><SlidersHorizontal class="mr-1.5 w-4" />Filter</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent class="w-56">
+        <DropdownMenuLabel>Status</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuCheckboxItem v-model:checked="showDone">
+            Show Done
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem v-model:checked="overdue">
+            Overdue
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuSeparator />
+        </DropdownMenuGroup>
+        <DropdownMenuLabel>Priorities</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem v-model:checked="critical">
+          Critical
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem v-model:checked="high">
+          High
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem v-model:checked="normal">
+          Normal
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem v-model:checked="low">
+          Low
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </div>
 </template>
