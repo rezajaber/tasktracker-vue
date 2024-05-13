0051562a6f7d7e9c750e8422343c9c2a8150a2aa
Reza Jaber <frontendrezajaber@gmail.com>
2024-05-13 11:32:31 +0200

    Organize Structe Of Project

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,9 +4,7 @@ import { CalendarClock } from "lucide-vue-next";
 import { Ellipsis } from "lucide-vue-next";
 import { ListChecks } from "lucide-vue-next";
 import { Trash2 } from "lucide-vue-next";
-
 import { Separator } from "@/components/ui/separator";
-
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -15,101 +13,44 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-const cards = [
-  {
-    color: "text-red-500",
-    category: "Cyber Security",
-    title: "Hack The Box",
-    content:
-      "Explore the latest trends in cybersecurity and learn how to protect your systems effectively.",
-    date: "22. September 2024",
+const props = defineProps({
+  priorityColor: {
+    type: String,
+    required: true,
   },
-  {
-    color: "text-blue-500",
-    category: "Data Analysis",
-    title: "Do not miss the Data",
-    content:
-      "Deep dive into data analysis techniques that can transform your business insights.",
-    date: "30. October 2024",
+  cardCategory: {
+    type: String,
+    required: true,
   },
-  {
-    color: "text-green-500",
-    category: "Renewable Energy",
-    title: "Tesla or Xiaomi",
-    content:
-      "Renewable energy solutions are becoming more affordable and accessible, driving sustainability.",
-    date: "15. November 2024",
+  cardTitle: {
+    type: String,
+    required: true,
   },
-  {
-    color: "text-yellow-500",
-    category: "Digital Marketing",
-    title: "Zahlungswerk to the Top",
-    content:
-      "Understanding the digital marketing landscape is crucial for any modern business strategy.",
-    date: "05. December 2024",
+  cardContent: {
+    type: String,
+    required: true,
   },
-  {
-    color: "text-purple-500",
-    category: "AI Development",
-    title: "ChatGPT - Dangerous?",
-    content:
-      "Artificial intelligence continues to evolve, offering new possibilities in various sectors.",
-    date: "10. January 2025",
+  cardDate: {
+    type: String,
+    required: true,
   },
-
-  {
-    color: "text-orange-500",
-    category: "Healthcare Innovation",
-    title: "AI-Doctors rule the world",
-    content:
-      "Innovative healthcare technologies are revolutionizing patient care worldwide.",
-    date: "25. February 2025",
-  },
-  /*{
-    color: "text-pink-500",
-    category: "Education Technology",
-    title: "New IPad-OS helps student",
-    content:
-      "The impact of technology on education is profound, shaping the future of learning.",
-    date: "20. March 2025",
-  },
-  {
-    color: "text-gray-500",
-    category: "Industry 4.0",
-    title: "Xiaomi Robots",
-    content:
-      "The fourth industrial revolution brings smart technology to manufacturing and production.",
-    date: "15. April 2025",
-  },
-  {
-    color: "text-indigo-500",
-    category: "Blockchain",
-    title: "Bitcoin over Etherum Bitcoin over EtherumBitcoin over Etherum",
-    content:
-      "Blockchain technology promises enhanced security and transparency in digital transactions.",
-    date: "05. May 2025",
-  },*/
-];
+});
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-5">
+  <div>
     <!-- FULL CARD -->
-    <div
-      v-for="card in cards"
-      :key="card.title"
-      class="card-container grid-rows-layout grid gap-3.5 rounded-lg p-5"
-    >
+    <div class="card-container grid-rows-layout grid gap-3.5 rounded-lg p-5">
       <!-- HEADER OF CARD -->
       <div class="flex items-center gap-2">
-        <Circle :class="`h-3 w-3 ${card.color} fill-current`" />
-        <span class="font-medium">{{ card.category }}</span>
+        <Circle :class="`h-3 w-3 ${priorityColor} fill-current`" />
+        <span class="font-medium">{{ cardCategory }}</span>
       </div>
 
       <!-- BODY OF CARD -->
       <div class="-mt-1 flex flex-col gap-1">
-        <h1 class="line-clamp-1 text-lg font-semibold">{{ card.title }}</h1>
-        <span class="line-clamp-3 text-sm">{{ card.content }}</span>
+        <h1 class="line-clamp-1 text-lg font-semibold">{{ cardTitle }}</h1>
+        <span class="line-clamp-3 text-sm">{{ cardContent }}</span>
       </div>
 
       <Separator class="w-full bg-gray-500" />
@@ -118,7 +59,7 @@ const cards = [
       <div class="flex items-center justify-between">
         <div class="flex items-center gap-2">
           <CalendarClock class="h-5 w-5" />
-          <span class="text-sm">{{ card.date }}</span>
+          <span class="text-sm">{{ cardDate }}</span>
         </div>
 
         <DropdownMenu>
@@ -152,13 +93,12 @@ const cards = [
 <style scoped>
 .card-container {
   width: 340px;
+  height: 220px;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 }
-
 .grid-rows-layout {
   grid-template-rows: auto 1fr auto auto;
 }
-
 .ellipsis-icon {
   stroke-width: 3px;
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,103 @@
 import SearchBar from "@/components/SearchBar.vue";
 import CardDetails from "@/components/CardDetails.vue";
 import Card from "@/components/Card.vue";
+
+const cards = [
+  {
+    priorityColor: "text-red-500",
+    cardCategory: "Cyber Security",
+    cardTitle: "Hack The Box",
+    cardContent:
+      "Explore the latest trends in cybersecurity and learn how to protect your systems effectively.",
+    cardDate: "22. September 2024",
+  },
+  {
+    priorityColor: "text-blue-500",
+    cardCategory: "Data Analysis",
+    cardTitle: "Do not miss the Data",
+    cardContent:
+      "Deep dive into data analysis techniques that can transform your business insights.",
+    cardDate: "30. October 2024",
+  },
+  {
+    priorityColor: "text-green-500",
+    cardCategory: "Renewable Energy",
+    cardTitle: "Tesla or Xiaomi",
+    cardContent:
+      "Renewable energy solutions are becoming more affordable and accessible, driving sustainability.",
+    cardDate: "15. November 2024",
+  },
+  {
+    priorityColor: "text-yellow-500",
+    cardCategory: "Digital Marketing",
+    cardTitle: "Zahlungswerk to the Top",
+    cardContent:
+      "Understanding the digital marketing landscape is crucial for any modern business strategy.",
+    cardDate: "05. December 2024",
+  },
+  {
+    priorityColor: "text-purple-500",
+    cardCategory: "AI Development",
+    cardTitle: "ChatGPT - Dangerous?",
+    cardContent:
+      "Artificial intelligence continues to evolve, offering new possibilities in various sectors.",
+    cardDate: "10. January 2025",
+  },
+
+  {
+    priorityColor: "text-orange-500",
+    cardCategory: "Healthcare Innovation",
+    cardTitle: "AI-Doctors rule the world",
+    cardContent:
+      "Innovative healthcare technologies are revolutionizing patient care worldwide.",
+    cardDate: "25. February 2025",
+  },
+  {
+    priorityColor: "text-pink-500",
+    cardCategory: "Education Technology",
+    cardTitle: "New IPad-OS helps student",
+    cardContent:
+      "The impact of technology on education is profound, shaping the future of learning.",
+    cardDate: "20. March 2025",
+  },
+  {
+    priorityColor: "text-gray-500",
+    cardCategory: "Industry 4.0",
+    cardTitle: "Xiaomi Robots",
+    cardContent:
+      "The fourth industrial revolution brings smart technology to manufacturing and production.",
+    cardDate: "15. April 2025",
+  },
+  {
+    priorityColor: "text-indigo-500",
+    cardCategory: "Blockchain",
+    cardTitle: "Bitcoin over Etherum Bitcoin over EtherumBitcoin over Etherum",
+    cardContent:
+      "Blockchain technology promises enhanced security and transparency in digital transactions. Blockchain technology promises enhanced security and transparency in digital transactions.",
+    cardDate: "05. May 2025",
+  },
+];
 </script>
 
 <template>
-  <div class="flex h-screen items-center justify-center">
-    <div class="grid gap-6">
-      <div class="grid gap-2">
-        <SearchBar />
-        <CardDetails />
-      </div>
-      <Card />
+  <div class="grid h-screen place-content-center place-items-center">
+    <div class="mb-6 flex w-full flex-col gap-2">
+      <SearchBar />
+      <CardDetails />
+    </div>
+
+    <div
+      class="no-scrollbar relative grid h-[560px] grid-cols-3 gap-5 overflow-y-auto rounded-lg p-2"
+    >
+      <Card
+        v-for="card in cards"
+        :key="card.cardTitle"
+        :priorityColor="card.priorityColor"
+        :cardCategory="card.cardCategory"
+        :cardTitle="card.cardTitle"
+        :cardContent="card.cardContent"
+        :cardDate="card.cardDate"
+      />
     </div>
   </div>
 </template>
